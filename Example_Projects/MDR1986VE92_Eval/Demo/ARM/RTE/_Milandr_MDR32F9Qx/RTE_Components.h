ad9c94373c7282dc3b4732c1e59eb99360d1f353
Vladislav Bogdanov <vla-bogdano@ya.ru>
2022-05-13 12:15:45 +0300

    test commit"

---
+++ Example_Projects/MDR1986VE92_Eval/Demo/ARM/RTE/_Milandr_MDR32F9Qx/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'MDR32F9Qx_Demo' 
+ * Target:  'Milandr MDR32F9Qx' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+
+#endif /* RTE_COMPONENTS_H */
